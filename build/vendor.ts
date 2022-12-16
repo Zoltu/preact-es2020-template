26ec425845433fe082ae3ddf6ed7d8a1bff9d51c
Micah Zoltu <micah@zoltu.net>
2022-12-16 16:04:11 +0800

    Removes es-module-shims.
    Firefox and Chcromium (i.e., Brave, Chrome, Edge) all support import maps natively now so we no longer need to use es-module-shims.
    
    Updated dependencies to latest while I was in here (just TypeScript and Preact).

--- build/vendor.ts
+++ build/vendor.ts
@@ -6,7 +6,6 @@ import { recursiveDirectoryCopy } from '@zoltu/file-copier'
 const directoryOfThisFile = path.dirname(url.fileURLToPath(import.meta.url))
 
 const dependencyPaths = [
-	{ packageName: 'es-module-shims', subfolderToVendor: 'dist', entrypointFile: 'es-module-shims.js' },
 	{ packageName: 'preact', subfolderToVendor: 'dist', entrypointFile: 'preact.module.js' },
 	{ packageName: 'preact/jsx-runtime', subfolderToVendor: 'dist', entrypointFile: 'jsxRuntime.module.js' },
 	{ packageName: 'preact/hooks', subfolderToVendor: 'dist', entrypointFile: 'hooks.module.js' },
