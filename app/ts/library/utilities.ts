e7f05a7e14f41f151cda6208da665eaadc11d6fb
Micah Zoltu <micah@zoltu.net>
2023-12-25 03:49:06 +0800

    Adds error ensuring helper.
    Adds pure helper function that will take in an `unknown` and give back an `Error`.  Useful in `catch` clauses when you want to deal with an `Error` and expect everything to be an `Error` but can't be sure.
    
    Also adds functions that sanely serialize `bigint` and `Uint8Array` to JSON (used in `ensureError(...)`.

--- app/ts/library/utilities.ts
+++ app/ts/library/utilities.ts
@@ -1,3 +1,27 @@
 export async function sleep(milliseconds: number) {
 	await new Promise(resolve => setTimeout(resolve, milliseconds))
 }
+
+export function jsonStringify(value: unknown, space?: string | number | undefined): string {
+    return JSON.stringify(value, (_key, value) => {
+		if (typeof value === 'bigint') return `0x${value.toString(16)}n`
+		if (value instanceof Uint8Array) return `b'${Array.from(value).map(x => x.toString(16).padStart(2, '0')).join('')}'`
+		return value
+    }, space)
+}
+export function jsonParse(text: string): unknown {
+	return JSON.parse(text, (_key: string, value: unknown) => {
+		if (typeof value !== 'string') return value
+		if (/^0x[a-fA-F0-9]+n$/.test(value)) return BigInt(value.slice(0, -1))
+		const bytesMatch = /^b'(:<hex>[a-fA-F0-9])+'$/.exec(value)
+		if (bytesMatch && 'groups' in bytesMatch && bytesMatch.groups && 'hex' in bytesMatch.groups) return hexToBytes(`0x${bytesMatch.groups['hex']}`)
+		return value
+	})
+}
+
+export function ensureError(caught: unknown) {
+	return (caught instanceof Error) ? caught
+		: typeof caught === 'string' ? new Error(caught)
+		: typeof caught === 'object' && caught !== null && 'message' in caught && typeof caught.message === 'string' ? new Error(caught.message)
+		: new Error(`Unknown error occurred.\n${jsonStringify(caught)}`)
+}
