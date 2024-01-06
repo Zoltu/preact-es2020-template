e3e1264db2e3937a60f8dbcd8f31963f153e45b4
Micah Zoltu <micah@zoltu.net>
2024-01-06 17:06:44 +0800

    fixup: json parsing.

--- app/ts/library/utilities.ts
+++ app/ts/library/utilities.ts
@@ -3,10 +3,11 @@ export async function sleep(milliseconds: number) {
 }
 
 export function jsonStringify(value: unknown, space?: string | number | undefined): string {
-    return JSON.stringify(value, (_key, value) => {
+    return JSON.stringify(value, (_, value) => {
 		if (typeof value === 'bigint') return `0x${value.toString(16)}n`
 		if (value instanceof Uint8Array) return `b'${Array.from(value).map(x => x.toString(16).padStart(2, '0')).join('')}'`
-		return value
+		// cast works around https://github.com/uhyo/better-typescript-lib/issues/36
+		return value as JSONValueF<unknown>
     }, space)
 }
 export function jsonParse(text: string): unknown {
@@ -14,7 +15,7 @@ export function jsonParse(text: string): unknown {
 		if (typeof value !== 'string') return value
 		if (/^0x[a-fA-F0-9]+n$/.test(value)) return BigInt(value.slice(0, -1))
 		const bytesMatch = /^b'(:<hex>[a-fA-F0-9])+'$/.exec(value)
-		if (bytesMatch && 'groups' in bytesMatch && bytesMatch.groups && 'hex' in bytesMatch.groups) return hexToBytes(`0x${bytesMatch.groups['hex']}`)
+		if (bytesMatch && 'groups' in bytesMatch && bytesMatch.groups && 'hex' in bytesMatch.groups && bytesMatch.groups['hex'].length % 2 === 0) return hexToBytes(`0x${bytesMatch.groups['hex']}`)
 		return value
 	})
 }
@@ -24,4 +25,12 @@ export function ensureError(caught: unknown) {
 		: typeof caught === 'string' ? new Error(caught)
 		: typeof caught === 'object' && caught !== null && 'message' in caught && typeof caught.message === 'string' ? new Error(caught.message)
 		: new Error(`Unknown error occurred.\n${jsonStringify(caught)}`)
+}
+
+function hexToBytes(value: string) {
+	const result = new Uint8Array((value.length - 2) / 2)
+	for (let i = 0; i < result.length; ++i) {
+		result[i] = Number.parseInt(value.slice(i * 2 + 2, i * 2 + 4), 16)
+	}
+	return result
 }
